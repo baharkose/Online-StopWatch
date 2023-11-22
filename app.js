4808c0a8b8f167cbe0ab0b2d2a9b8297c71da364
baharkose <baharkse17@gmail.com>
2023-11-22 19:47:40 +0300

    added js codes

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ const centiSecond = document.querySelector(".splite-second")
 const startBtn = document.getElementById("start")
 const stopBtn = document.getElementById("stop")
 const clearBtn = document.getElementById("clear")
+
+
+let centiSecond = 0;
+const countCentiSecond = ()=>{
+    setInterval()
+}
+
+
+startBtn.addEventListener("click", ()=>{
+
+})
