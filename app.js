e5cfb534d67173cebffd9d78120dea4258b1fd4c
baharkose <baharkse17@gmail.com>
2023-11-22 19:14:54 +0300

    fixed times alignment

---
+++ app.js
@@ -0,0 +1,10 @@
+const hour = document.querySelector(".hour")
+const minute = document.querySelector(".minute")
+const second = document.querySelector(".minute")
+const centiSecond = document.querySelector(".splite-second")
+
+// Buttons
+
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const clearBtn = document.getElementById("clear")
